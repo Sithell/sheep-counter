cd976be75d686a8510344463ec9d5b7db89e11be
Sagitav Sagitov <thesithell@gmail.com>
2025-05-04 21:39:28 +0300

    rollback CORS

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ app = FastAPI(
 # Настройка CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Разрешаем запросы с фронтенда
+    allow_origins=["http://localhost:5173"],  # Разрешаем запросы с фронтенда
     allow_credentials=True,
     allow_methods=["*"],  # Разрешаем все методы
     allow_headers=["*"],  # Разрешаем все заголовки
